ce87cf8c2fcdba1f61a76598148e77fe4caf4550
haitienwork <haitien.work@gmail.com>
2025-11-06 18:53:19 +0700

    implement filter functionality in Filter component and enhance FilterButton with dropdown selection

--- components/home/Filter.tsx
+++ components/home/Filter.tsx
@@ -1,38 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import FilterButton from "./FilterButton";
 
 export default function Filter() {
+	const [rank, setRank] = useState("");
+	const [price, setPrice] = useState("");
+	const [skin, setSkin] = useState("");
+	const [champ, setChamp] = useState("");
+	const [level, setLevel] = useState("");
+
+	const rankOptions = [
+		{ value: "cao-thu", label: "Cao thủ" },
+		{ value: "bac-kim", label: "Bạch Kim" },
+		{ value: "vang", label: "Vàng" },
+		{ value: "bach-kim", label: "Bạch Kim" },
+		{ value: "xanh", label: "Xanh" },
+		{ value: "luc-bao", label: "Lục bảo" },
+		{ value: "bac-bao", label: "Bạc bảo" },
+		{ value: "vang-bao", label: "Vàng bảo" },
+		{ value: "do-bao", label: "Đỏ bảo" },
+		{ value: "xanh-bao", label: "Xanh bảo" },
+	];
+
 	return (
 		<View>
 			<View style={styles.container}>
 				<FilterButton
 					placeholder="Tìm theo rank"
-					onPress={() => {}}
 					style={styles.filterButton}
+					options={rankOptions}
+					selectedValue={rank}
+					onSelectValue={setRank}
 				/>
 				<FilterButton
 					placeholder="Tìm theo giá"
-					onPress={() => {}}
 					style={styles.filterButton}
 				/>
 			</View>
 			<View style={styles.container}>
 				<FilterButton
 					placeholder="Tìm theo trang phục"
-					onPress={() => {}}
 					style={styles.filterButton}
 				/>
 				<FilterButton
 					placeholder="Tìm theo skin"
-					onPress={() => {}}
-					style={styles.filterButton}
-				/>
-			</View>
-      <View style={styles.container}>
-				<FilterButton
-					placeholder="Tìm theo bậc"
-					onPress={() => {}}
 					style={styles.filterButton}
 				/>
 			</View>
--- components/home/FilterButton.tsx
+++ components/home/FilterButton.tsx
@@ -1,49 +1,127 @@
 import { colors } from "@/libs/colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import type React from "react";
-import {
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	type ViewStyle,
-} from "react-native";
+import { useState } from "react";
+import { Text, TouchableOpacity, View, type ViewStyle } from "react-native";
 
 interface FilterButtonProps {
 	placeholder: string;
-	onPress: () => void;
 	style?: ViewStyle;
 	options?: {
 		value: string;
 		label: string;
 		icon?: React.ReactNode;
-	};
+	}[];
+	selectedValue?: string;
+	onSelectValue?: (value: string) => void;
 }
 
 export default function FilterButton(props: FilterButtonProps) {
-	const { placeholder, onPress, style } = props;
+	const { placeholder, style } = props;
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handlePress = () => {
+		setIsOpen((prev) => !prev);
+	};
+
+	const getSelectedValueColor = (value: string) => {
+		return props.selectedValue === value ? colors["lol-gold"] : "white";
+	};
+
+	const onSelectValue = (value: string) => {
+		if (props.selectedValue === value) {
+			props.onSelectValue?.("");
+		} else {
+			props.onSelectValue?.(value);
+		}
+
+		setIsOpen(false);
+	};
+
+	const displayValue = props.selectedValue
+		? props.options?.find((option) => option.value === props.selectedValue)
+				?.label
+		: placeholder;
 
 	return (
-		<TouchableOpacity style={[styles.container, style]} onPress={onPress}>
-			<Text style={styles.placeholder}>{placeholder}</Text>
-			<Ionicons name="chevron-down" size={20} color={colors["lol-gold"]} />
-		</TouchableOpacity>
+		<View
+			style={{
+				position: "relative",
+				...style,
+			}}
+		>
+			<TouchableOpacity
+				style={{
+					flexDirection: "row",
+					alignItems: "center",
+					gap: 8,
+					padding: 8,
+					borderWidth: 1,
+					borderColor: colors["lol-gold"],
+					backgroundColor: "#000B0D",
+					borderRadius: 6,
+					justifyContent: "space-between",
+					...(props.selectedValue && {
+						backgroundColor: `${colors["lol-gold"]}50`,
+					}),
+				}}
+				onPress={handlePress}
+			>
+				<Text
+					style={{
+						fontSize: 14,
+						color: "white",
+					}}
+				>
+					{displayValue}
+				</Text>
+				<Ionicons name="chevron-down" size={20} color={colors["lol-gold"]} />
+			</TouchableOpacity>
+			{isOpen && (
+				<View
+					style={{
+						position: "absolute",
+						top: "100%",
+						left: 0,
+						right: 0,
+						backgroundColor: colors["card"],
+						borderRadius: 6,
+						padding: 8,
+						borderWidth: 1,
+						borderColor: colors["lol-gold"],
+						gap: 8,
+						zIndex: 1,
+					}}
+				>
+					{props.options?.map((option) => (
+						<TouchableOpacity
+							key={option.value}
+							onPress={() => onSelectValue(option.value)}
+							style={{
+								flexDirection: "row",
+								alignItems: "center",
+								justifyContent: "space-between",
+								gap: 8,
+							}}
+						>
+							<Text
+								style={{
+									color: getSelectedValueColor(option.value),
+								}}
+							>
+								{option.label}
+							</Text>
+							{props.selectedValue === option.value && (
+								<Ionicons
+									name="checkmark"
+									size={20}
+									color={colors["lol-gold"]}
+								/>
+							)}
+						</TouchableOpacity>
+					))}
+				</View>
+			)}
+		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flexDirection: "row",
-		alignItems: "center",
-		gap: 8,
-		padding: 8,
-		borderWidth: 1,
-		borderColor: colors["lol-gold"],
-		backgroundColor: "#000B0D",
-		borderRadius: 6,
-		justifyContent: "space-between",
-	},
-	placeholder: {
-		fontSize: 14,
-		color: "white",
-	},
-});
